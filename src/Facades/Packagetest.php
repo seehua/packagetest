3b31edcb8ad53fb8ebca872b9188970e63f7125a
seehua <seehua@qq.com>
2019-01-16 15:54:03 +0800

    Signed-off-by: seehua <seehua@qq.com>

---
+++ src/Facades/Packagetest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Seehua\Packagetest\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Packagetest extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'packagetest';
+    }
+}
